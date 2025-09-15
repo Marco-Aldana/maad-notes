703b4eb1ee6a3a5b71cd1ff46442ba69de535c0c
Marco Aldana <marco.aldana.d@outlook.com>
2025-09-15 10:03:31 -0600

    Add app icon (temp)

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="default" size="sm">
+        Delete
+      </Button>
+    </div>
+  );
+}
